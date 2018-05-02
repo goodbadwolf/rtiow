23eccf53fda18f6a54015b62f7e678b4d3f729fe
Manish Mathai <mmathai@uoregon.edu>
2018-05-02 01:08:18 -0700

    Add vec3 ray

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <fstream>
 
+
+#include "vec3.h"
+
 int main()
 {
   int width = 200;
@@ -12,12 +15,12 @@ int main()
   {
     for(int i = 0; i < width; ++i)
     {
-      float r = static_cast<float>(i) / static_cast<float>(width);
-      float g = static_cast<float>(j) / static_cast<float>(height);
-      float b = 0.2f;
-      output << static_cast<int>(255.99f * r) << " " 
-             << static_cast<int>(255.99f * g) << " "
-             << static_cast<int>(255.99f * b) << "\n";
+      vec3 color(static_cast<float>(i) / static_cast<float>(width), 
+                 static_cast<float>(j) / static_cast<float>(height),
+                 0.2f);
+      output << static_cast<int>(255.99f * color[0]) << " " 
+             << static_cast<int>(255.99f * color[1]) << " "
+             << static_cast<int>(255.99f * color[2]) << "\n";
     }
   }
 }
